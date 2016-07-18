c792658e5e9348fb6d5e6d10f9d7af1929876b54
Yale Song <yalesong@yahoo-inc.com>
2016-07-18 12:17:09 -0400

    Enable matlab openmp on linux

--- matlab/compile.m
+++ matlab/compile.m
@@ -12,7 +12,7 @@ linux=strcmp(arch,'GLNXA64') || strcmp(arch,'GLNX86');
 sixtyfourbits=strcmp(arch,'MACI64') || strcmp(arch,'GLNXA64') || strcmp(arch,'PCWIN64');
 
 debug=false;
-use_multithread=false;
+use_multithread=true;
 use_64bits_integers=false;
 
 if debug 
